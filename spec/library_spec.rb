85de3a80893a148862118846dbcfc06545e50c17
KaraJH <karas.jones@yahoo.com>
2022-12-23 12:39:20 -0700

    test: return method

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -86,4 +86,49 @@ RSpec.describe Library do
 
     end
   end
+
+  describe '#return' do 
+    it 'removes the checkout out book from the checked out list of books' do 
+      dpl = Library.new("Denver Public Library")  
+
+      charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
+      jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")    
+      professor = charlotte_bronte.write("The Professor", "1857")    
+      villette = charlotte_bronte.write("Villette", "1853")    
+
+      harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})    
+
+      mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960") 
+
+      expect(dpl.checkout(mockingbird)).to eq(false)
+      expect(dpl.checkout(jane_eyre)).to eq(false)
+
+      dpl.add_author(charlotte_bronte)
+      dpl.add_author(harper_lee) 
+
+      expect(dpl.checkout(jane_eyre)).to eq(true)
+      expect(dpl.checked_out_books).to eq([jane_eyre])
+
+      expect(dpl.checkout(jane_eyre)).to eq(false)
+
+      dpl.return(jane_eyre) 
+      expect(dpl.checked_out_books).to eq([])
+      expect(dpl.checkout(jane_eyre)).to eq(true)
+      expect(dpl.checkout(villette)).to eq(true)
+      expect(dpl.checked_out_books).to eq([jane_eyre, villette])
+      expect(dpl.checkout(mockingbird)).to eq(true)
+
+      dpl.return(mockingbird)
+
+      expect(dpl.checkout(mockingbird)).to eq(true)
+
+      dpl.return(mockingbird) 
+
+      expect(dpl.checkout(mockingbird)).to eq(true)
+
+      #expect(dpl.most_popular_book).to eq(mockingbird)
+
+    end
+  end
 end
