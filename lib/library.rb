b618685d02a54b4da2fed49a55026e0a32cadffd
KaraJH <karas.jones@yahoo.com>
2022-12-23 12:10:38 -0700

    feat: Add author with passing test

--- lib/library.rb
+++ lib/library.rb
@@ -6,4 +6,28 @@ class Library
     @books = []
     @authors = []
   end
+
+  def add_author(author)
+    
+    @authors << author
+    @books += get_author_books(author)
+
+    #  add_books(author)
+    #  @books.flatten
+    
+    # why can't I do this? calling .flatten on @books
+    # works in pry. But the test is returning a nested array? 
+    # consider me confused
+  end
+
+  # def add_books(author)
+  #   @books << get_author_books(author)
+  # end
+
+  def get_author_books(author) 
+    author.books.map do |book| 
+      book
+    end
+  end
+    
 end
