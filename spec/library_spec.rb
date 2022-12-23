ec7372e1b5e0afa20bfba33784138d8d106b669f
KaraJH <karas.jones@yahoo.com>
2022-12-23 12:29:14 -0700

    test: Checkout and checked out methods

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -58,4 +58,32 @@ RSpec.describe Library do
       expect(dpl.publication_time_frame_for(harper_lee)).to eq({:start=>"1960", :end=>"1960"})
     end
   end
+
+  describe '#checkout' do 
+    it 'returns true if the book can be checked out or false if it is already checked out or not in the library' do 
+      dpl = Library.new("Denver Public Library")  
+
+      charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
+      jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")    
+      professor = charlotte_bronte.write("The Professor", "1857")    
+      villette = charlotte_bronte.write("Villette", "1853")    
+
+      harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})    
+
+      mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960") 
+
+      expect(dpl.checkout(mockingbird)).to eq(false)
+      expect(dpl.checkout(jane_eyre)).to eq(false)
+
+      dpl.add_author(charlotte_bronte)
+      dpl.add_author(harper_lee) 
+
+      expect(dpl.checkout(jane_eyre)).to eq(true)
+      expect(dpl.checked_out_books).to eq([jane_eyre])
+
+      dpl.checkout(jane_eyre).to eq(false)
+
+    end
+  end
 end
